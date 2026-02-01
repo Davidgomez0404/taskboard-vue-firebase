c3e377d03d6cb22ce84db673d3d47b18173fd5a0
DavidGomezGimeno <davidgg04@gmail.es>
2026-02-01 15:04:21 +0100

    Cambios finales

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,18 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkyEXCO-Yrl_ikFPuHReUvPjMw8EKvTck",
-  authDomain: "taskboard-david.firebaseapp.com",
-  projectId: "taskboard-david",
-  storageBucket: "taskboard-david.firebasestorage.app",
-  messagingSenderId: "50048732139",
-  appId: "1:50048732139:web:ce19bbf03cf5650b8fe7ae"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+console.log("API Key cargada:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 const app = initializeApp(firebaseConfig);
-// Exportar Auth y Firestore
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
